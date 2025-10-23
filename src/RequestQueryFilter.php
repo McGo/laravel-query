b92ea0a8763aeb49639f5f007e5e982bc5ced0db
GitLab CI <ci@example.com>
2025-10-23 16:33:44 +0200

    initial commit

---
+++ src/RequestQueryFilter.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace McGo\RequestQueryFilter;
+
+class RequestQueryFilter
+{
+
+}
---
+++ src/RequestQueryFilterServiceProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace McGo\RequestQueryFilter;
+
+use Illuminate\Support\ServiceProvider;
+
+class RequestQueryFilterServiceProvider extends ServiceProvider
+{
+
+}
